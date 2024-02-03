d174251389e9d6528ac68018932c389d53182c40
Asim <asimpsy11@gmail.com>
2024-02-03 13:02:35 +0100

    Mango db connection done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import exppress from "express";
 import { PORT } from "./config.js";
+import { MangoDBURL } from "./test.js";
+import mongoose from "mongoose";
 
 const app = exppress();
 
@@ -13,6 +15,16 @@ app.get("/favicon.ico", (request, response) => {
   return response.status(204);
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to: ${PORT}`);
-});
+mongoose
+  .connect(MangoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+
+    // Server only runs when database connection is successful
+    app.listen(PORT, () => {
+      console.log(`App is listening to: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
